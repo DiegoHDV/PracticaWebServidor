f688a1bc0bf77a932fc54b303872d6be86227c9d
DiegoHDV <diegoherranzdelvalle@gmail.com>
2025-04-09 09:32:52 +0200

    Configuración de BD en función de si haremos tests o no

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-
+const db_uri = process.env.NODE_ENV === 'test' ? process.env.DB_URI : process.env.DB_URI_TEST
+/*
 const dbConnect = () => {
     const db_uri = process.env.DB_URI
     mongoose.set('strictQuery', false)
@@ -10,6 +11,10 @@ const dbConnect = () => {
     }
     // Listen events
     mongoose.connection.on("connected", () => console.log("Conectado a la base de datos"))
+}*/
+const dbConnect = () => {
+    mongoose.set('strictQuery', false)
+    mongoose.connect(db_uri)
 }
 
 module.exports = dbConnect
